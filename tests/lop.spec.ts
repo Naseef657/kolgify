db1293e9e5070d965e979317d2e63d42b53878a4
NaseefG8TS <naseef.mottath@G8TS.com>
2026-06-28 09:37:16 +0300

    test update: 2026-06-28 09:37

---
+++ tests/lop.spec.ts
@@ -0,0 +1,19 @@
+import { getFormattedDate,getFormattedDateOnly,CustomgetFormattedDate,getCurrentDate } from '../../utils.js';
+import { test, expect, devices } from '@playwright/test';
+
+test.use({
+  ignoreHTTPSErrors: true,
+  storageState: './user-auth.json'
+});
+
+test('test_TC-D1-00009', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#tomselect-1-ts-control').click();
+  await page.getByRole('option', { name: '🇦🇪 United Arab Emirates' }).last().click();
+  await page.goto('/?country=3');
+  await page.getByText('BarrysTDubai').click();
+  await page.waitForTimeout(4000);
+  await page.getByRole('button', { name: 'Cancel' }).click();
+  await page.getByRole('button', { name: 'yess' }).click();
+  await page.goto('/event/249053?_from=user_calendar&date=2026-06-14');
+});
